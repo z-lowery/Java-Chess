36514fd00c4cd41efb2047de70db84df9ef00cb2
z-lowery <z.lowery@outlook.com>
2025-01-10 16:10:46 -0600

    Chaged name of App.java -> Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import Board.Chessboard;
 
 import java.io.IOException;
 
-public class App {
+public class Main {
     public static void main(String[] args) throws IOException {
         new Chessboard();
     }
